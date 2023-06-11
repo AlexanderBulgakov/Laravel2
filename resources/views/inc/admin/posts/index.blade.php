1ed815984b3fc4aab4a7161f1cce71ad9a1ce986
Alexander <alexander@localhost.loc>
2023-06-11 19:50:25 +0300

    index post

--- resources/views/inc/admin/posts/index.blade.php
+++ resources/views/inc/admin/posts/index.blade.php
@@ -3,6 +3,21 @@
         {{ __('Posts') }}
     </x-slot>
 
-    index
+    <table class="border-collapse table-auto w-full text-sm mb-4">
+        <thead>
+            <tr class="border-b border-gray-300">
+                <th class="p-4 text-left">{{ __('ID') }}</th>
+                <th class="p-4 text-left">{{ __('Title') }}</th>
+            </tr>
+        </thead>
+        <tbody class="bg-white dark:bg-slate-800">
+            @foreach($posts as $post)
+                <tr class="border-b border-gray-300">
+                    <td class="p-4">{{ $post->id }}</td>
+                    <td class="p-4"><a href="{{ route('posts.show', $post->id) }}" class="hover:underline">{{ $post->title }}</a></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 </x-app-layout>
