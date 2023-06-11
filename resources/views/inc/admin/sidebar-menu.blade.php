e965d8b6976817d9f0c788e7a2002ef18185267c
Alexander <alexander@localhost.loc>
2023-06-11 17:10:02 +0300

    upd route, added prefix

--- resources/views/inc/admin/sidebar-menu.blade.php
+++ resources/views/inc/admin/sidebar-menu.blade.php
@@ -1,4 +1,6 @@
 <ul>
     <li><a href="#">Dashboard</a></li>
     <li><a href="#">Posts</a></li>
+    <li><a href="#">Profile</a></li>
+    <li><a href="#">Log out</a></li>
 </ul>
--- routes/web.php
+++ routes/web.php
@@ -30,15 +30,15 @@ Route::get('/blog', function () {
     return view('blog');
 })->name('blog');
 
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::prefix('dashboard')->middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
