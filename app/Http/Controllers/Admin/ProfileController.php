baed2f17aab137196765f7b8f371669baea6f695
Alexander <alexander@localhost.loc>
2023-06-24 15:41:38 +0300

    fixed prodile

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -17,7 +17,7 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
-        return view('inc.admin.profile.edit', [
+        return view('admin.profile.edit', [
             'user' => $request->user(),
         ]);
     }
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -4,15 +4,15 @@
     </x-slot>
 
     <div class="p-4 border-b">
-        @include('inc.admin.profile.partials.update-profile-information-form')
+        @include('admin.profile.partials.update-profile-information-form')
     </div>
 
     <div class="p-4 border-b">
-        @include('inc.admin.profile.partials.update-password-form')
+        @include('admin.profile.partials.update-password-form')
     </div>
 
     <div class="p-4">
-        @include('inc.admin.profile.partials.delete-user-form')
+        @include('admin.profile.partials.delete-user-form')
     </div>
 
 </x-app-layout>
