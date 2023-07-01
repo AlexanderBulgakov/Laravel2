d12ba7e7210c4ee042e25ffa9b949c15f96062b0
Alexander <alexander@localhost.loc>
2023-07-01 16:37:47 +0300

    upd blog design

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,15 +2,21 @@
     <x-slot:title>
         {{ __('Blog page') }}
     </x-slot>
- 
-    <h1 class="block text-3xl font-bold mb-4">{{ __('Blog') }}</h1>
+
+    <h1 class="block text-3xl font-bold mb-4 text-center">{{ __('Blog') }}</h1>
     @if($posts->isNotEmpty())
-        @foreach($posts as $post)
-            <article class="my-4">
-                <h2 class="text-2xl font-bold pb-4"><a href="{{ route('blog.show', $post->slug) }}" class="hover:underline">{{ $post->title }}</a></h2>
-                <p class="mb-4 text-sm">{{ $post->description }}</p>
-            </article>
-        @endforeach
+        <div class="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            @foreach($posts as $post)
+                <a href="{{ route('blog.show', $post->slug) }}" class="w-full p-2 hover:bg-gray-100 hover:rounded sm:block">
+                    <img role="presentation" class="object-cover w-full rounded h-44" src="https://source.unsplash.com/random/480x360?5">
+                    <div class="pt-2 space-y-2">
+                        <h2 class="text-xl">{{ $post->title }}</h2>
+                        <span class="text-xs dark:text-gray-400">TODO</span>
+                        <p>{{ $post->description }}</p>
+                    </div>
+                </a>
+            @endforeach
+        </div>
     @else
         <p>{{ __('No posts available') }}</p>
     @endif
