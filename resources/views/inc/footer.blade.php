96d71fa219f20d7f7e2f23f954cd667defd3765e
Alexander <alexander@localhost.loc>
2023-07-15 16:26:48 +0300

    added calendar in footer menu

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,6 +2,7 @@
     <div class="flex flex-col sm:flex-row items-center justify-center text-sm py-6">
         <x-nav-footer-link :href="route('home')" :active="request()->routeIs('home')">{{ __('Home') }}</x-nav-footer-link>
         <x-nav-footer-link :href="route('blog')" :active="request()->routeIs('blog*') || request()->routeIs('category.show')">{{ __('Blog') }}</x-nav-footer-link>
+        <x-nav-footer-link :href="route('calendar')" :active="request()->routeIs('calendar')">{{ __('Calendar') }}</x-nav-footer-link>
         <x-nav-footer-link :href="route('about')" :active="request()->routeIs('about')">{{ __('About') }}</x-nav-footer-link>
         <x-nav-footer-link :href="route('contact')" :active="request()->routeIs('contact')">{{ __('Contact') }}</x-nav-footer-link>
     </div>
